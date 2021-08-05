e9d030610b6647ce2e735dcd3401726eef243247
chLi <565353780@qq.com>
2021-08-05 15:17:31 +0800

    remove LoadImages dependency

--- detect.py
+++ detect.py
@@ -1,12 +1,13 @@
+import os
 import time
-from pathlib import Path
+import numpy as np
 
+from PIL import Image
 import cv2
 from numpy import random
 import jittor as jt
 
 from models.yolo import Model
-from utils.datasets import LoadImages
 from utils.general import check_img_size, non_max_suppression, scale_coords, \
     xyxy2xywh, set_logging, increment_path
 from utils.plots import plot_one_box
@@ -15,8 +16,40 @@ import models.common as common
 
 jt.flags.use_cuda=1
 
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True):
+    # Resize image to a 32-pixel-multiple rectangle https://github.com/ultralytics/yolov3/issues/232
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, 32), np.mod(dh, 32)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
 def detect(save_img=False):
-    imgsz = 640
+    imgsz = 416
 
     # Load model
     model = Model("./configs/yolov3.yaml")
@@ -25,9 +58,6 @@ def detect(save_img=False):
     model.eval()
 
     imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
-
-    # Set Dataloader
-    dataset = LoadImages("./sample_images", img_size=imgsz)
 
     # Get names and colors
     names = model.names
@@ -38,7 +68,13 @@ def detect(save_img=False):
     image_file_name_list = os.listdir(image_folder_path)
 
     for image_file_name in image_file_name_list:
-        img = cv2.imread(os.path.join(image_folder_path, image_file_name))
+        img_source = cv2.imread(os.path.join(image_folder_path, image_file_name))
+
+        img = letterbox(img_source, new_shape=(640, 640))[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
+        img = jt.array(img,dtype="float32") # uint8 to fp32
         img /= 255.0
         if img.ndim == 3:
             img = img.unsqueeze(0)
@@ -51,7 +87,9 @@ def detect(save_img=False):
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            im0 = im0s
+            im0 = img_source
+
+            detect_result = ""
 
             gn = jt.array([im0.shape[1],im0.shape[0],im0.shape[1],im0.shape[0]]).float32()  # normalization gain whwh
             if len(det):
@@ -61,68 +99,20 @@ def detect(save_img=False):
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
+                    detect_result += str(int(n)) + " " + names[int(c.int())] + ", "
                 
                 # Write results
                 for i in reversed(range(len(det))):
                     xyxy = det[i,:4]
                     conf = det[i,4]
                     cls = det[i,5].int()
-                    #  if save_txt:  # Write to file
-                    #      xywh = (xyxy2xywh(jt.array(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    #      line = (cls, *xywh, conf) # label format
-                    #      with open(txt_path + '.txt', 'a') as f:
-                    #          f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                    #  if save_img or view_img:  # Add bbox to image
-                    #      label = f'{names[int(cls)]} {conf:.2f}'
-                    #      plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
-            #  cv2.imshow(str(p), im0)
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
-
-    # Run inference
-    for path, img, im0s, vid_cap in dataset:
-        img = jt.array(img,dtype="float32") # uint8 to fp32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndim == 3:
-            img = img.unsqueeze(0)
-
-        # Inference
-        pred = model(img, augment=False)[0]
-
-        # Apply NMS
-        pred = non_max_suppression(pred, 0.25, 0.45, None, False)
-
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            im0 = im0s
-
-            gn = jt.array([im0.shape[1],im0.shape[0],im0.shape[1],im0.shape[0]]).float32()  # normalization gain whwh
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                
-                # Write results
-                for i in reversed(range(len(det))):
-                    xyxy = det[i,:4]
-                    conf = det[i,4]
-                    cls = det[i,5].int()
-                    #  if save_txt:  # Write to file
-                    #      xywh = (xyxy2xywh(jt.array(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    #      line = (cls, *xywh, conf) # label format
-                    #      with open(txt_path + '.txt', 'a') as f:
-                    #          f.write(('%g ' * len(line)).rstrip() % line + '\n')
-                    #  if save_img or view_img:  # Add bbox to image
-                    #      label = f'{names[int(cls)]} {conf:.2f}'
-                    #      plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-
-            #  cv2.imshow(str(p), im0)
-
-    print(f'Done. ({time.time() - t0:.3f}s)')
-
+            #  cv2.imshow("test", im0)
+            #  cv2.waitKey(0)
+            print(detect_result)
 
 if __name__ == '__main__':
     common.Conv.use_v3 = True
